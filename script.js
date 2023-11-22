33f74f5e788c131ad4b16a26f27e232e70be95cd
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-22 21:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function Person(name, age) {
 }
 Person.prototype.greet= function()
 	{
-		console.log(`Hello, my name is ${name}`);
-		console.log(`I am ${age} years old`);
+		console.log(`Hello, my name is ${this.name}`);
+		console.log(`I am ${this.age} years old`);
 	}
 function Employee(name, age, jobTitle) 
 {
@@ -18,11 +18,13 @@ Employee.prototype.__proto__=Person.prototype;
 Employee.prototype.jobGreet = function()
 	{
 		greet().call(this);
-		console.log(`my job title is ${jobTitle}`)
+		console.log(`my job title is ${this.jobTitle}`)
 	}
 // Do not change code below this line
 window.Person = Person;
-window.Employee = Employee;
+window.Employee = Employee
+
+
 
 
 
