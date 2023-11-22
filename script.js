ba81dde845e9bb692789f8260ce94663f9a5feb2
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-22 21:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ function Person(name, age) {
 
 Person.prototype.greet = function () 
 {
-  console.log("Hello, my name is ${this.name}");
-  console.log("I am ${this.age} years old");
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
 };
 
 function Employee(name, age, jobTitle) {
