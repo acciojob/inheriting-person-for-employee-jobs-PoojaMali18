08631ecc33dd3d7d75f347e543098a18315539b4
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-22 21:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 // complete this js code
 function Person(name, age) {
-	this.name=name;
-	this.age=age;
-}
-Person.prototype.greet= function()
-	{
-		console.log(`Hello, my name is ${this.name}`);
-		console.log(`I am ${this.age} years old`);
-	}
-function Employee(name, age, jobTitle) 
-{
-   Person.call(this,name,age);
-	this.jobTitle=jobTitle;
+  this.name = name;
+  this.age = age;
 }
 
-Employee.prototype.__proto__=Person.prototype;
-Employee.prototype.jobGreet = function()
-	{
-		this.greet()
-		console.log(`my job title is ${this.jobTitle}`)
-	}
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}`);
+  console.log(`I am ${this.age} years old`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype =Person.prototype;
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+  this.greet();
+  console.log(`My job title is ${this.jobTitle}`);
+};
+
 // Do not change code below this line
 window.Person = Person;
-window.Employee = Employee
-
+window.Employee = Employee;
 
 
 
