f4cc806665e524b0f0f624d1fdfb9e62291bd9e9
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2024-02-13 21:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-// complete this js code
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-Person.prototype.greet = function () 
+class person
 {
-console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
-};
-
-function Employee(name, age, jobTitle) {
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+    constructor(name,age)
+    {
+        this._name=name;
+        this._age=age;
+    }
+    
+    get name()
+    {
+        return this._name;
+    }
+    
+    get age()
+    {
+        return this._age;
+    }
+    
+    greet()
+    {
+console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`)
+    }
 }
 
-Employee.prototype.__proto__ = Person.prototype;
-Employee.prototype.constructor = Employee;
 
-Employee.prototype.jobGreet = function () {
-//  Person.prototype.greet.call(this); // Call the greet method from Person prototype
-   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 
-};
 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+
+class employee extends person
+{
+    constructor(name,age,jobTitle)
+    {
+        super(name,age);
+        this._jobTitle=jobTitle;
+    }
+
+     
+    jobGreet()
+    {
+        console.log(`Hello, my name is ${this._name}>, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+    }
+}
+
+
+
+
+
+const per=new employee('pooja',21,'SDE')
+per.jobGreet();
+per.greet();
+
+
+
 
 
 
